e0609f3e40c46725573923423aa986c4a5136dea
Andras Lasso <lasso@queensu.ca>
2019-05-21 19:31:21 -0400

    Update CMakeLists.txt to allow testing with VTK master (VTK8.90)
    Also added #defines to choose between various configuration options.

--- renderbug.cpp
+++ renderbug.cpp
@@ -4,18 +4,32 @@
 #include <QtDebug>
 
 #include <QVTKOpenGLWidget.h>
+#include <QVTKOpenGLNativeWidget.h>
 
 #include <vtkGenericOpenGLRenderWindow.h>
 #include <vtkNew.h>
 #include <vtkRenderer.h>
+#include <vtkVersion.h>
+
+///////////////////////////////////////////////
+// Comment/uncomment the following lines to
+// experiment with different configurations:
+
+#define USE_NATIVE_WIDGET
+//#define USE_COMPATIBILITY_PROFILE
+
+///////////////////////////////////////////////
 
 int main(int argc, char *argv[])
 {
-    auto format = QVTKOpenGLWidget::defaultFormat();
+    auto fmt = QVTKOpenGLWidget::defaultFormat();
 
-    //format.setProfile(QSurfaceFormat::CompatibilityProfile);
-
-    QSurfaceFormat::setDefaultFormat(format);
+#ifdef USE_COMPATIBILITY_PROFILE
+    qDebug() << "Use compatibility profile";
+    format.setProfile(QSurfaceFormat::CompatibilityProfile);
+#endif
+    
+    QSurfaceFormat::setDefaultFormat(fmt);
 
     QApplication app(argc, argv);
 
@@ -24,14 +38,27 @@ int main(int argc, char *argv[])
     vtkNew<vtkGenericOpenGLRenderWindow> window;
     window->AddRenderer(renderer);
 
+#ifdef USE_NATIVE_WIDGET
+    QVTKOpenGLNativeWidget widget;
+#else
     QVTKOpenGLWidget widget;
-    widget.SetRenderWindow(window);
+#endif
 
-    QTimer::singleShot(2000, [&window, &renderer]() {
+#if VTK_MAJOR_VERSION >= 9 || (VTK_MAJOR_VERSION >= 8 && VTK_MINOR_VERSION >= 3)
+    widget.setRenderWindow(window);
+#else
+    widget.SetRenderWindow(window);
+#endif
+
+    QTimer::singleShot(3000, [&window, &renderer]() {
         qDebug() << "Setting background to red and rendering";
         renderer->SetBackground(1.0, 0.0, 0.0);
         window->Render();
-        //window->Render();
+    });
+
+    QTimer::singleShot(6000, [&window, &renderer]() {
+      qDebug() << "Render again";
+      window->Render();
     });
 
     widget.show();
---
+++ start_dbg.bat
@@ -0,0 +1,2 @@
+set path=c:\D\VTK-bin\bin\Debug;c:\Qt\5.10.0\msvc2015_64\bin;%path%
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe" c:\D\RenderingUpdateError_R\renderbug.sln
